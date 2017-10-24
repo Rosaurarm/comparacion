1142909db7bd62207dd3d498a863ba2337173f73
Rosaura <rosaurarm@prodigy.net.mx>
2017-10-24 09:15:29 -0500

    hola subiendo mi primer README.md

---
+++ apps.js
@@ -0,0 +1,10 @@
+alert("hola  chicas");
+var comparar =function(arreglo1, arreglo2){ // paso 1
+
+  document.write('Si son iguales');
+
+}
+var array1 = [1,2,3,4,5,6,7,8,9,10];
+var array2 = [1,2,3,4,5,6,7,8,9,10]
+
+comprar (array1, array2);
